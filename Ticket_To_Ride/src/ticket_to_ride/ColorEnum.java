4e22f58d513e4c64ca06168696f5a9ab229d9eb1
samk2020 <44036369+samk2020@users.noreply.github.com>
2018-11-14 11:03:53 -0800

    Update ColorEnum.java

--- Ticket_To_Ride/src/ticket_to_ride/ColorEnum.java
+++ Ticket_To_Ride/src/ticket_to_ride/ColorEnum.java
@@ -1,6 +1,11 @@
 //samk2020
 public enum ColorEnum { 
     //needs more editing
-    RED, BLUE, GREEN, YELLOW, ORANGE, PURPLE, BLACK, WHITE, WILDCARD; 
+    RED, BLUE, GREEN, YELLOW, ORANGE, PURPLE, BLACK, WHITE, WILDCARD;  
+    private static ColorEnum [] allValues = values();
+
+    public static ColorEnum fromOrg(int x) { 
+        return allValues[x]; 
+    }
 }
  
