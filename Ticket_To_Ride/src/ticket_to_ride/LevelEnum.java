6eb768887282fbb3e4329621b7ebc4d3e04387ad
samk2020 <44036369+samk2020@users.noreply.github.com>
2018-11-28 12:13:53 -0800

    Delete LevelEnum.java

--- Ticket_To_Ride/src/ticket_to_ride/LevelEnum.java
+++
@@ -1,7 +0,0 @@
-//samk2020 
-//DISREGARD THIS FILE
-public enum LevelEnum { 
-    //needs more editing 12 cards on each color
-    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, ELEVEN, TWELVE; 
-
-}
