45a7f51f40ab75fcd9dcb06f8448823b7e73a418
samk2020 <44036369+samk2020@users.noreply.github.com>
2018-11-28 12:14:12 -0800

    Delete DestEnum.java

--- Ticket_To_Ride/src/ticket_to_ride/DestEnum.java
+++
@@ -1,6 +0,0 @@
-//disregard this file...to be deleted soon and replaced by location enum file
-public enum DestEnum {
-    DEST1, DEST2, DEST3, DEST4, DEST5, DEST6, DEST7, DEST8, DEST9, DEST10, DEST11,
-    DEST12, DEST13, DEST14, DEST15, DEST16, DEST17, DEST18, DEST19, DEST20, DEST21,
-    DEST22, DEST23, DEST24, DEST25, DEST26, DEST27, DEST28, DEST29, DEST30;
-}
