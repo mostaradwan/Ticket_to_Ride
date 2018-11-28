2e198fa856088fe31169d9d774ff154d2b510adc
samk2020 <44036369+samk2020@users.noreply.github.com>
2018-11-28 10:13:12 -0800

    Add files via upload
    this is replacement(updated) for dest enum dot java file

---
+++ Ticket_To_Ride/src/ticket_to_ride/LocationEnum.java
@@ -0,0 +1,44 @@
+//samk2020
+public enum LocationEnum { 
+    // list of locations needed for the use of player destination
+    ATLANTA,
+    BOSTON,
+    CALGARY,
+    CHARLESTON,
+    CHICAGO,
+    DALLAS,
+    DENVER,
+    DULUTH,
+    EL_PASO,
+    HELENA,
+    HOUSTON,
+    KANSAS_CITY,
+    LAS_VEGAS,
+    LITTLE_ROCK,
+    LOS_ANGELES,
+    MIAMI,
+    MONTREAL,
+    NASHVILLE,
+    NEW_ORLEANS,
+    NEW_YORK,
+    OKLAHOMA_CITY,
+    OMAHA,
+    PHOENIX,
+    PITTSBURG,
+    PORTLAND,
+    RALEIGH,
+    SAINT_LOUIS,
+    SALT_LAKE_CITY,
+    SAN_FRANCISCO,
+    SANTA_FE,
+    SAULT_ST_MARIE,
+    SEATTLE,
+    TORONTO,
+    VANCOUVER,
+    WASHINGTON,
+    WINNIPEG;
+
+    private static LocationEnum[] allValues = values();
+    public static LocationEnum fromOrdinal(int n) { return allValues[n]; }
+}
+
