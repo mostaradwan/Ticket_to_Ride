11e0a5c5dd542bf603aca5cd7b325cfcc6253e91
samk2020 <44036369+samk2020@users.noreply.github.com>
2018-12-06 11:01:03 -0800

    might not need this because of updates
    to be removed/replaced

--- Ticket_To_Ride/src/ticket_to_ride/EmptyDeckException.java
+++ Ticket_To_Ride/src/ticket_to_ride/EmptyDeckException.java
@@ -1,4 +1,4 @@
-//samk2020
+//samk2020 idk if we gonna need this after updates that i haven't finalized yet
 public class EmptyDeckException extends Exception{
     //needs more editing
     public EmptyDeckException(Throwable cause){
